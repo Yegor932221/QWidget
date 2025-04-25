a817c47a23f3071fd02e9073230d135ca19e2a50
Yegor <Yegor@HOME-PC.local>
2025-04-25 13:50:25 +0700

    Сделал русскую локализацию виджета

--- win.cpp
+++ win.cpp
@@ -1,46 +1,52 @@
 #include "win.h"
 #include <QValidator>
 
-win::win(QWidget *parent):QWidget(parent)
+win::win(QWidget *parent) : QWidget(parent)
 {
-    codec = QTextCodec::codecForName("Windows-1251");
-    setWindowTitle(codec->toUnicode("Возведение в квадрат"));
+    // Установка кодировки UTF-8
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
+    setWindowTitle(QString::fromUtf8("Возведение в квадрат"));
+
     frame = new QFrame(this);
     frame->setFrameShadow(QFrame::Raised);
     frame->setFrameShape(QFrame::Panel);
-    inputLabel = new QLabel(codec->toUnicode("Введите число:"),
-                            this);
-    inputEdit = new QLineEdit("",this);
-    StrValidator *v=new StrValidator(inputEdit);
+
+    inputLabel = new QLabel(QString::fromUtf8("Введите число:"), this);
+    inputEdit = new QLineEdit("", this);
+
+    StrValidator *v = new StrValidator(inputEdit);
     inputEdit->setValidator(v);
-    outputLabel = new QLabel(codec->toUnicode("Результат:"),
-                             this);
-    outputEdit = new QLineEdit("",this);
-    nextButton = new QPushButton(codec->toUnicode("Следующее"),
-                                 this);
-    exitButton = new QPushButton(codec->toUnicode("Выход"),
-                                 this);
-    // компоновка приложения выполняется согласно рисунку 2
+
+    outputLabel = new QLabel(QString::fromUtf8("Результат:"), this);
+    outputEdit = new QLineEdit("", this);
+
+    nextButton = new QPushButton(QString::fromUtf8("Следующее"), this);
+    exitButton = new QPushButton(QString::fromUtf8("Выход"), this);
+
+    // Компоновка приложения
     QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
     vLayout1->addWidget(inputLabel);
     vLayout1->addWidget(inputEdit);
     vLayout1->addWidget(outputLabel);
     vLayout1->addWidget(outputEdit);
     vLayout1->addStretch();
+
     QVBoxLayout *vLayout2 = new QVBoxLayout();
     vLayout2->addWidget(nextButton);
     vLayout2->addWidget(exitButton);
     vLayout2->addStretch();
+
     QHBoxLayout *hLayout = new QHBoxLayout(this);
     hLayout->addWidget(frame);
     hLayout->addLayout(vLayout2);
+
     begin();
-    connect(exitButton,SIGNAL(clicked(bool)),
-            this,SLOT(close()));
-    connect(nextButton,SIGNAL(clicked(bool)),
-            this,SLOT(begin()));
-    connect(inputEdit,SIGNAL(returnPressed()),
-            this,SLOT(calc()));
+
+    // Подключение сигналов и слотов с современным синтаксисом
+    connect(exitButton, &QPushButton::clicked, this, &QWidget::close);
+    connect(nextButton, &QPushButton::clicked, this, &win::begin);
+    connect(inputEdit, &QLineEdit::returnPressed, this, &win::calc);
 }
 
 void win::begin()
