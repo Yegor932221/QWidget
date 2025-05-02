6a3f87115dd9b38176a018686847bcfa0b46e82e
Yegor <Yegor@HOME-PC.local>
2025-05-02 12:48:39 +0700

    Перенёс всё в папку

---
+++ Generating_new_signals/win.cpp
@@ -0,0 +1,35 @@
+#include "win.h"
+#include <QValidator>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QTextCodec>
+
+Win::Win(QWidget *parent):QWidget(parent)
+{
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+    codec = QTextCodec::codecForName("Windows-1251");
+    this->setWindowTitle(QString::fromUtf8("Счетчик"));
+    label1 = new QLabel(QString::fromUtf8("Cчет по 1"),this);
+    label2 = new QLabel(QString::fromUtf8("Cчет по 5"),this);
+    edit1 = new Counter("0",this);
+    edit2 = new Counter("0",this);
+    calcbutton=new QPushButton("+1",this);
+    exitbutton=new QPushButton(QString::fromUtf8("Выход"),this);
+    QHBoxLayout *layout1 = new QHBoxLayout();
+    layout1->addWidget(label1);
+    layout1->addWidget(label2);
+    QHBoxLayout *layout2 = new QHBoxLayout();
+    layout2->addWidget(edit1);
+    layout2->addWidget(edit2);
+    QHBoxLayout *layout3 = new QHBoxLayout();
+    layout3->addWidget(calcbutton);
+    layout3->addWidget(exitbutton);
+    QVBoxLayout *layout4 = new QVBoxLayout(this);
+    layout4->addLayout(layout1);
+    layout4->addLayout(layout2);
+    layout4->addLayout(layout3);
+    // связь сигнала нажатия кнопки и слота закрытия окна
+    connect(calcbutton,SIGNAL(clicked(bool)),edit1,SLOT(add_one()));
+    connect(edit1,SIGNAL(tick_signal()), edit2,SLOT(add_one()));
+    connect(exitbutton,SIGNAL(clicked(bool)),this,SLOT(close()));
+}
